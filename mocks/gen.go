508b6a165d98cffd4cf881dedaaa63535a41e075
Mark Wolfe <mark@wolfe.id.au>
2022-04-01 16:15:58 +1100

    chore(deps): upgrade all my deps and uplift makefile

--- mocks/gen.go
+++ mocks/gen.go
@@ -1,5 +1,4 @@
 // Package mocks provides mock implementations for AWS services
 package mocks
 
-//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/golang/mock/mockgen
-//go:generate $PWD/.bin/mockgen -destination awsmock/ssm.go -package awsmock github.com/aws/aws-sdk-go/service/ssm/ssmiface SSMAPI
+//go:generate go run github.com/golang/mock/mockgen -destination awsmock/ssm.go -package awsmock github.com/aws/aws-sdk-go/service/ssm/ssmiface SSMAPI
